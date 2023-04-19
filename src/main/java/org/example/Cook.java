4c1ca5657dfa82cb5ebddd31f9b0bc4d4b64caf6
alan <kno12350@gmail.com>
2023-04-19 21:18:49 +0900

    요리 주문에 관한 테스트 코드 작성 시작

---
+++ src/main/java/org/example/Cook.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Cook {
+    private final String food;
+    private final int price;
+
+    public Cook(String food, int price) {
+        this.food = food;
+        this.price = price;
+    }
+}
