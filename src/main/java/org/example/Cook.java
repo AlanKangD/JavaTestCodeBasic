0d1d20c64377218c05d4324579ea9d0fb78523fc
alan <kno12350@gmail.com>
2023-04-19 21:36:21 +0900

    객체들 끼리 비교 후 체크하기 위해서는 equals and hashCode 를 추가해줍니다

--- src/main/java/org/example/Cook.java
+++ src/main/java/org/example/Cook.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Cook {
     private final String food;
     private final int price;
@@ -7,5 +9,21 @@ public class Cook {
     public Cook(String food, int price) {
         this.food = food;
         this.price = price;
+    }
+
+    // 만약 객체들끼리 비교 하기 위해서는 equals 그리고 hashCode라는 것을 추가해주면 되는데 commend + n을 누르면 추가 할 수 있다.
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cook cook = (Cook) o;
+        return price == cook.price && Objects.equals(food, cook.food);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(food, price);
     }
 }
---
+++ src/main/java/org/example/MenuItem.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class MenuItem {
+    private final String food;
+    private final int price;
+
+    public MenuItem(String food, int price) {
+        this.food = food;
+        this.price = price;
+
+    }
+}
